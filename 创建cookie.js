256b1ad09cfd4c0b317516c9b64a27032b3cd029
xuechangsheng <xiaxueme@gmail.com>
2017-01-17 16:17:33 +0800

    创建cookie

---
+++ 创建cookie.js
@@ -0,0 +1,17 @@
+//创建cookie
+function setCookie(name, value, expires, path, domain, secure) {
+    var cookieText = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+    if (expires instanceof Date) {
+        cookieText += '; expires=' + expires;
+    }
+    if (path) {
+        cookieText += '; expires=' + expires;
+    }
+    if (domain) {
+        cookieText += '; domain=' + domain;
+    }
+    if (secure) {
+        cookieText += '; secure';
+    }
+    document.cookie = cookieText;
+}
