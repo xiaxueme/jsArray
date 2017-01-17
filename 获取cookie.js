7a9a60d35525915faf78babf067b74414ff10aba
踏薛寻梅 <xiaxueme@users.noreply.github.com>
2017-01-17 16:39:48 +0800

    Create 获取cookie.js

---
+++ 获取cookie.js
@@ -0,0 +1,14 @@
+//获取cookie
+function getCookie(name) {
+    var cookieName = encodeURIComponent(name) + '=';
+    var cookieStart = document.cookie.indexOf(cookieName);
+    var cookieValue = null;
+    if (cookieStart > -1) {
+        var cookieEnd = document.cookie.indexOf(';', cookieStart);
+        if (cookieEnd == -1) {
+            cookieEnd = document.cookie.length;
+        }
+        cookieValue = decodeURIComponent(document.cookie.substring(cookieStart + cookieName.length, cookieEnd));
+    }
+    return cookieValue;
+}
